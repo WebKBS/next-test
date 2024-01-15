5cb57410b4aaa1b3e6fe51005b6a1ff58757a9f8
gangbeomsu <kbs21c1987@gmail.com>
2024-01-15 23:40:55 +0900

    간단한 스타일 추가

--- intersection-observer/components/observer.tsx
+++ intersection-observer/components/observer.tsx
@@ -11,12 +11,24 @@ export default function Observer() {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
+          // 화면에 보일 때만 스케일과 컬러를 추가
+          (entry.target as HTMLElement).style.transform = 'scale(1.8)';
+          (entry.target as HTMLElement).style.backgroundColor = 'red';
+          (entry.target as HTMLElement).style.transition = 'all 1s';
           console.log(
-            `${entry.target.textContent} 엘리먼트가 뷰포트 안에 있습니다!`
+            `${
+              (entry.target as HTMLElement).textContent
+            } 엘리먼트가 뷰포트 안에 있습니다!`
           );
         } else {
+          // 화면에 보이지 않을 때 초기 스타일로 설정
+          // 만약 필요하지 않을시 else문은 삭제해도 됨
+          (entry.target as HTMLElement).style.transform = 'scale(1)';
+          (entry.target as HTMLElement).style.backgroundColor = 'gray';
           console.log(
-            `${entry.target.textContent} 엘리먼트가 뷰포트 밖에 있습니다!`
+            `${
+              (entry.target as HTMLElement).textContent
+            } 엘리먼트가 뷰포트 밖에 있습니다!`
           );
         }
       });
@@ -33,7 +45,7 @@ export default function Observer() {
         observer.unobserve(element);
       });
     };
-  }, []); // Empty dependency array means this effect will run once on mount
+  }, []);
 
   const items = Array.from({ length: 100 }, (_, i) => i + 1);
   return (
@@ -46,7 +58,7 @@ export default function Observer() {
               width: '300px',
               height: '300px',
               backgroundColor: 'gray',
-              marginBottom: '50px',
+              marginBottom: '400px',
             }}
           >
             {item}
